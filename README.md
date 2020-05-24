097be34a7769dcf1318c090f6eea1999bf9ae731
shuliuzhenhua <971270272@qq.com>
2020-05-24 12:11:38 +0800

    init

---
+++ README.md
@@ -0,0 +1,30 @@
+# 在HbuilderX中自动将px单位转化成rpx单位
+
+## 使用方式
+
+>1. 下载并解压后。安装到 `HbuilderX` 中的插件目录，然后重启即可
+     mac 目录 `/Applications/HBuilderX.app/Contents/HBuilderX/plugins/`
+>2. 在编辑器中右键，选择 px2rpx，或者 关闭 px2rpx
+>3. 试试改动代码然后保存
+
+
+## 示例
+
+>1. 自动转化 emmet 语法
+```css
+# 按下tab键保存试试
+div {
+    w20+h20
+}
+```
+>2. 蓝湖复制来的代码自动转化
+```css
+div {
+	width: 20px;
+	height: 20px;
+	border-radius: 6px;
+}
+```
+
+## 感谢
+>1. [多功能翻译插件](https://gitee.com/cloverzx/translation)
---
+++ extension.js
@@ -0,0 +1,68 @@
+var hx = require("hbuilderx");
+const diff = require("./diff.js")
+//该方法将在插件激活的时候调用
+function activate(context) {
+    let disposable = hx.commands.registerCommand('extension.px2rpx', () => {
+		let config = hx.workspace.getConfiguration();
+		sets("close", "0","px2rpx开启成功！！！!")
+        let onDidChangeTextDocumentEventDispose = hx.workspace.onWillSaveTextDocument(function(event) {
+			let close = config.get("close", false);
+			console.log(close)
+			if (Number.parseInt(close) > 0) {
+				return false;
+			}
+            let doc = event.document;
+            let fileName = doc.fileName;
+            if (fileName.search('.less')) {
+            	console.log(fileName)
+            	let text = doc.getText();
+            	let newtext = text.replace(/(?<=\d+)px/g, 'rpx');
+            	const diffs = diff.stringDiff(text, newtext, false);
+            	let workspaceEdit = new hx.WorkspaceEdit();
+            	let edits = [];
+            	for (let df of diffs) {
+            		edits.push(new hx.TextEdit({
+            			start: df.originalStart,
+            			end: df.originalStart + df.originalLength
+            		}, newtext.substr(df.modifiedStart, df.modifiedLength)));
+            	}
+            	workspaceEdit.set(doc.uri, edits);
+            	hx.workspace.applyEdit(workspaceEdit);
+            }
+        });
+    });
+	let setbaidu = hx.commands.registerCommand('extension.closePx2rpx', () => {
+		sets("close", "1","px2rpx关闭成功!")
+	});
+	
+	
+    //订阅销毁钩子，插件禁用的时候，自动注销该command。
+    context.subscriptions.push(disposable);
+}
+//该方法将在插件禁用的时候调用（目前是在插件卸载的时候触发）
+function deactivate() {
+
+}
+
+
+/**
+ * 设置配置,给予反馈
+ * @description 设置配置,给予反馈
+ * @param {Number} type 
+ * @param {String} data
+ * @param {String} desc 文字信息 
+ */
+function sets(type,data,desc){
+	hx.workspace.getConfiguration().update(type, data)
+		.then(() => {
+			hx.window.showInformationMessage(desc)
+		})
+		.catch(() => {
+			hx.window.showWarningMessage("设置失败!")
+		});
+}
+
+module.exports = {
+    activate,
+    deactivate
+}
